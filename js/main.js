384577a39f6b2585613435313bf65e090043767d
Akeyla Virgin <a.virgin05@icloud.com>
2022-07-27 20:12:42 -0400

    first xommit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.querySelector('#submit').addEventListener('onclick', myFunction )
+
+function myFunction() {
+    alert("Your request has been submitted, be on the lookout for an email to complete your purchase!")
+}
