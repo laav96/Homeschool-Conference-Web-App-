fe875e1a6574db155fb1b477400ccd33ba647fa5
Akeyla Virgin <a.virgin05@icloud.com>
2022-09-14 19:33:23 -0400

    first commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
+const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".nav-menu");
+const items = document.querySelectorAll(".nav-item");
+
+/* Toggle mobile menu */
+function toggleMenu(){
+    if (menu.classList.contains("active")){
+        menu.classList.remove("active");
+
+        // adds the menu (hamburger) icon
+        toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+    } else{
+        menu.classList.add("active");
+
+        // adds the close (x) icon
+        toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+    }
+}
+
+/* Event listener */
+toggle.addEventListener("click", toggleMenu, false);
+for (let item of items){
+    if (item.querySelector(".submenu")){
+        item.addEventListener("click", toggleItem, false);
+    }
+        item.addEventListener("keypress", toggleItem, false);
+    }
+document.addEventListener("click", closeSubmenu, false);
+
 document.querySelector('#submit').addEventListener('onclick', myFunction )
 
 function myFunction() {
